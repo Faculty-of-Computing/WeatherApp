0d77096fa63cc109fefbb40e67bc625b328c59a6
Gideon Jones <109116843+Gideonjon@users.noreply.github.com>
2025-08-31 16:22:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-export FLASK_APP=server:app
-
-if [ ! -d "migrations" ]; then
-    flask db init
-fi
-
-flask db migrate -m "Auto migration"
-flask db upgrade
